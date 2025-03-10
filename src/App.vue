05bb609acdce7af3d8be59195ccef315efddd130
aliciadewet <alicia.dewet@gmail.com>
2025-03-10 13:45:44 +0000

    :recycle: move movie into movie item component

--- src/App.vue
+++ src/App.vue
@@ -3,29 +3,11 @@ import { computed, reactive, ref } from "vue";
 /*
 These are Icons that you can use, of course you can use other ones if you prefer.
 */
-import { StarIcon, TrashIcon, PencilIcon } from "@heroicons/vue/24/solid";
+
 import { items } from "./movies.json";
+import MovieItem from "@/MovieItem.vue";
 
 const movies = ref(items);
-
-function updateRating(movieIndex, rating) {
-  movies.value[movieIndex].rating = rating;
-}
-function removeMovie(movieIndex) {
-  movies.value = movies.value.filter((movie, index) => index !== movieIndex);
-}
-function editMovie(movieIndex) {
-  const movie = movies.value[movieIndex];
-
-  form.id = movie.id;
-  form.name = movie.name;
-  form.description = movie.description;
-  form.image = movie.image;
-  form.inTheaters = movie.inTheaters;
-  form.genres = movie.genres;
-
-  showForm();
-}
 
 const errors = reactive({
   name: null,
@@ -143,6 +125,7 @@ function clearErrors() {
 }
 
 const showMovieForm = ref(false);
+
 function hideForm() {
   showMovieForm.value = false;
   cleanUpForm();
@@ -283,84 +266,8 @@ function removeRatings() {
       </div>
     </div>
     <div class="movie-list">
-      <div
-        class="movie-item group"
-        v-for="(movie, movieIndex) in movies"
-        :key="movie.id"
-      >
-        <div class="movie-item-image-wrapper">
-          <div class="movie-item-star-wrapper">
-            <StarIcon
-              id="rating"
-              class="movie-item-star-rating-icon"
-              :class="[movie.rating ? 'text-yellow-500' : 'text-gray-500']"
-            />
-            <div class="movie-item-star-content-wrapper">
-              <span
-                v-if="movie.rating"
-                id="rating-stars"
-                class="movie-item-star-content-rating-rated"
-              >
-                {{ movie.rating }}
-              </span>
-              <span v-else class="movie-item-star-content-rating-not-rated">
-                -
-              </span>
-            </div>
-          </div>
-          <img :src="movie.image" class="movie-item-image" alt="" />
-        </div>
-
-        <div class="movie-item-content-wrapper">
-          <div class="movie-item-title-wrapper">
-            <h3 class="movie-item-title">{{ movie.name }}</h3>
-            <div class="movie-item-genres-wrapper">
-              <span
-                v-for="genre in movie.genres"
-                :key="`${movie.id}-${genre}`"
-                class="movie-item-genre-tag"
-                >{{ genre }}</span
-              >
-            </div>
-          </div>
-          <div class="movie-item-description-wrapper">
-            <p class="movie-item-description">{{ movie.description }}</p>
-          </div>
-          <div class="movie-item-rating-wrapper">
-            <span class="movie-item-rating-text">
-              Rating: ({{ movie.rating }}/5)
-            </span>
-            <div class="movie-item-star-icon-wrapper">
-              <button
-                v-for="star in 5"
-                :key="star"
-                class="movie-item-star-icon-button"
-                :class="[
-                  star <= movie.rating ? 'text-yellow-500' : 'text-gray-500',
-                ]"
-                :disabled="star === movie.rating"
-                @click="updateRating(movieIndex, star)"
-              >
-                <StarIcon class="movie-item-star-icon" />
-              </button>
-            </div>
-
-            <div class="movie-item-actions-list-wrapper">
-              <button
-                class="movie-item-action-edit-button"
-                @click="editMovie(movieIndex)"
-              >
-                <PencilIcon class="w-4 h-4" />
-              </button>
-              <button
-                class="movie-item-action-remove-button"
-                @click="removeMovie(movieIndex)"
-              >
-                <TrashIcon class="w-4 h-4" />
-              </button>
-            </div>
-          </div>
-        </div>
+      <div class="movie-item group" v-for="movie in movies" :key="movie.id">
+        <MovieItem :movie />
       </div>
     </div>
   </div>
