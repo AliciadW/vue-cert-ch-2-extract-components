680889422684d906c84d736658557cd33a189e20
aliciadewet <alicia.dewet@gmail.com>
2025-04-06 13:45:57 +0100

    :package: Add vue router

--- src/main.js
+++ src/main.js
@@ -1,4 +1,24 @@
 import { createApp } from "vue";
+import { createRouter, createWebHistory } from "vue-router";
 import App from "./App.vue";
+import MovieView from "@/pages/MovieView.vue";
+import MovieList from "@/pages/MovieList.vue";
 import "../style.css";
-createApp(App).mount("#app");
+
+const routes = [
+  {
+    path: "/",
+    component: MovieList,
+  },
+  {
+    path: "/:id",
+    component: MovieView,
+  },
+];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
+
+createApp(App).use(router).mount("#app");
